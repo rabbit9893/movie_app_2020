be9cbcb81d74a254fa4c41dd8cbfb69035acda36
rabbit9893 <rabbit9893@naver.com>
2020-12-11 20:26:40 +0900

    2-2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React from 'react';
 
-function Food({fav}) {
+function Food({name, picture}) {
   {/* {fav} == props.fav와 같음? props.fav를 하는 것보다 {fav}를 하는 것이 더 효율적임?*/ }
-  console.log({fav});
-  return <h1>I like {fav}</h1>;
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} width="250px" height="250px"/>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name : "Kimchi",
+    image: "http://web.tradekorea.com/product/987/1664987/Whole%20Cabbage%20Kimchi%20_1.jpg"
+  },
+  {
+    name : "Samgyeopsal",
+    image: "http://www.blaud.co.kr/uploads/fw1604005/201909/4419185ba11a7cc2b1d5f54363f2b0eb.jpg"
+  },
+  {
+    name : "Bibimbap",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F74%2F13%2F92%2F7413925459deee0499336273d4f7a4c7.jpg&f=1&nofb=1"
+  },
+  {
+    name : "Doncasu",
+    image: "https://jungdaham.com/web/product/big/201804/31_shop1_189698.jpg"
+  },
+  {
+    name : "Kimbap",
+    image: "https://static.myletter.net/20190225/edd1e01f-4502-453b-8b61-688941aefb76.jpg"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" /> 
-      <Food fav="ramen" /> 
-      <Food fav="samgiopsal" /> 
-      <Food fav="chukumi" /> 
-      {/* 누군가가 food component로 정보를 보내려고 하면 react는 모든 속성을 가져옴 
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} /> 
+      ))}
+      {/* dish : 각각의 item(object) */}
+      {/* <Food fav="kimchi" />
+          누군가가 food component로 정보를 보내려고 하면 react는 모든 속성을 가져옴 
           => food function 컴포넌트의 agrument(인자)로 넣음 */}
     </div>
   );
