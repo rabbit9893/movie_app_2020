09c09c8fb586ee50129e2c713f3038f49dac576b
rabbit9893 <rabbit9893@naver.com>
2020-12-11 20:15:35 +0900

    2-1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({fav}) {
+  {/* {fav} == props.fav와 같음? props.fav를 하는 것보다 {fav}를 하는 것이 더 효율적임?*/ }
+  console.log({fav});
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" /> 
+      <Food fav="ramen" /> 
+      <Food fav="samgiopsal" /> 
+      <Food fav="chukumi" /> 
+      {/* 누군가가 food component로 정보를 보내려고 하면 react는 모든 속성을 가져옴 
+          => food function 컴포넌트의 agrument(인자)로 넣음 */}
     </div>
   );
 }
 
+{/* App에서 food 컴포넌트로 정보를 보내고 그 다음 food 컴포넌트에서 그 정보를
+어떻게 사용하는지 */}
 export default App;
