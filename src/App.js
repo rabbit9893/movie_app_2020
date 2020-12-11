707bdbda16d9f499fd623f8c26a1f65b483b2e65
rabbit9893 <rabbit9893@naver.com>
2020-12-11 20:44:48 +0900

    2-3 map Recap

--- src/App.js
+++ src/App.js
@@ -5,41 +5,53 @@ function Food({name, picture}) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src={picture} width="250px" height="250px"/>
+      <img src={picture} width="250px" height="250px" alt={name} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id : 1,
     name : "Kimchi",
     image: "http://web.tradekorea.com/product/987/1664987/Whole%20Cabbage%20Kimchi%20_1.jpg"
   },
   {
+    id : 2,
     name : "Samgyeopsal",
     image: "http://www.blaud.co.kr/uploads/fw1604005/201909/4419185ba11a7cc2b1d5f54363f2b0eb.jpg"
   },
   {
+    id : 3,
     name : "Bibimbap",
     image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F74%2F13%2F92%2F7413925459deee0499336273d4f7a4c7.jpg&f=1&nofb=1"
   },
   {
+    id : 4,
     name : "Doncasu",
     image: "https://jungdaham.com/web/product/big/201804/31_shop1_189698.jpg"
   },
   {
+    id : 5,
     name : "Kimbap",
     image: "https://static.myletter.net/20190225/edd1e01f-4502-453b-8b61-688941aefb76.jpg"
   }
 ];
 
+/*
+function renderFood(dish) {
+  return <Food name={dish.name} picture={dish.image} />
+}
+*/
+
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} /> 
+        <Food key={dish.id} name={dish.name} picture={dish.image} /> 
       ))}
-      {/* dish : 각각의 item(object) */}
+      {/* foodILike.map(renderFood) => 위에 있는 것 대신 함수를 새로 만들어서 할 수 있음*/}
+      {/* dish : 각각의 item(object) key : react 내부에서 사용하기 위한 것 */}
       {/* <Food fav="kimchi" />
           누군가가 food component로 정보를 보내려고 하면 react는 모든 속성을 가져옴 
           => food function 컴포넌트의 agrument(인자)로 넣음 */}
