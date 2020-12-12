46280e77a873b30fce60f60494b370c34e1b6d63
rabbit9893 <rabbit9893@naver.com>
2020-12-12 15:15:25 +0900

    3-1 Class Components and State (setState)

--- src/App.js
+++ src/App.js
@@ -1,76 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id : 1,
-    name : "Kimchi",
-    image : "http://web.tradekorea.com/product/987/1664987/Whole%20Cabbage%20Kimchi%20_1.jpg",
-    rating : 5
-  },
-  {
-    id : 2,
-    name : "Samgyeopsal",
-    image : "http://www.blaud.co.kr/uploads/fw1604005/201909/4419185ba11a7cc2b1d5f54363f2b0eb.jpg",
-    rating : 4.9
-  },
-  {
-    id : 3,
-    name : "Bibimbap",
-    image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F74%2F13%2F92%2F7413925459deee0499336273d4f7a4c7.jpg&f=1&nofb=1",
-    rating : 4.8
-  },
-  {
-    id : 4,
-    name : "Doncasu",
-    image : "https://jungdaham.com/web/product/big/201804/31_shop1_189698.jpg",
-    rating : 5.5
-  },
-  {
-    id : 5,
-    name : "Kimbap",
-    image : "https://static.myletter.net/20190225/edd1e01f-4502-453b-8b61-688941aefb76.jpg",
-    rating : 4.7
+class App extends React.Component {
+  /* state 안에는 바꾸고 싶은 data를 넣음 */
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+    /* this.state.count + 1은 좋지 않은 방법, current로 현재의 state를 가져와서 current.count로  */
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        {/* this.add() => 이거은 function, 클릭했을 때만 function이 호출되기를 원하기 때문(즉시가 아니라) ()는 즉시를 나타냄 */}
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-function Food({name, picture, rating}) {
-  {/* {fav} == props.fav와 같음? props.fav를 하는 것보다 {fav}를 하는 것이 더 효율적임?*/ }
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} width="250px" height="250px" alt={name} />
-    </div>
-  );
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-/*
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image} />
-}
-*/
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} /> 
-      ))}
-      {/* foodILike.map(renderFood) => 위에 있는 것 대신 함수를 새로 만들어서 할 수 있음*/}
-      {/* dish : 각각의 item(object) key : react 내부에서 사용하기 위한 것 */}
-      {/* <Food fav="kimchi" />
-          누군가가 food component로 정보를 보내려고 하면 react는 모든 속성을 가져옴 
-          => food function 컴포넌트의 agrument(인자)로 넣음 */}
-    </div>
-  );
-}
-
-{/* App에서 food 컴포넌트로 정보를 보내고 그 다음 food 컴포넌트에서 그 정보를
-어떻게 사용하는지 */}
 export default App;
