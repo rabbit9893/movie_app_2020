88ab7b3b0b07f2ac04ad13c0e3b039eb6204d760
rabbit9893 <rabbit9893@naver.com>
2020-12-13 13:31:27 +0900

    4-0 Fetching Moives from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
-  /* render 하면 호출되는 life cycle method(mount 되자마자 호출됨) */
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
   componentDidMount() {
-    // => 이곳에서 data를 fetch함 (API로부터 data fetching 완료 -> movie를 render하고 map을 만들고 movie를 render)
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
