1b94763b164808b6ff800f3c9005873bf3e4b9ef
rabbit9893 <rabbit9893@naver.com>
2020-12-14 14:19:34 +0900

    6-4 Redirecting

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,21 +4,21 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 // Movie 컴포넌트는 state가 필요없으므로 클래스 컴포넌트가 될 필요 없음
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <Link
-      to={{
-        pathname: "/movie-detail",
-        state: {
-          year,
-          title,
-          summary,
-          poster,
-          genres
-        }
-      }}
-    >
-      <div className="movie">
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+          }
+        }}
+      >
         <img src={poster} alt={title} title={title} />
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
@@ -32,8 +32,8 @@ function Movie({ year, title, summary, poster, genres }) {
           </ul>
           <p className="movie__summary">{summary.slice(0, 180)}</p>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
 
